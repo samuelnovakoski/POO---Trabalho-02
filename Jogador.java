7c62f7c98152649d12e877d09bc10481a2f2b557
Anderson Lesniewski <anderson.lesniews.pb@compasso.com.br>
2023-11-18 16:53:54 -0300

    Update v.0.0.2: adicionados métodos em Jogador

--- Jogador.java
+++ Jogador.java
@@ -4,19 +4,27 @@ public abstract class Jogador {
     private String nome;
     private JogoDados[] jogos = new JogoDados[10];
     private int jogo = 0;
+    private int tipo;
 
     public Jogador(){
         setNome();
     }
 
-    public Jogador(String nome){
+    public Jogador(String nome, int tipo){
         this.nome = nome;
+        this.tipo = tipo;
     }
 
     public void setNome(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Insira o nome do jogador: ");
         this.nome = scan.nextLine();
+    }
+
+    public void setTipo(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insira o tipo de jogador (1 - Humano; 2 - Maquina): ");
+        this.tipo = scan.nextLine();
     }
 
     public String getNome(){
@@ -27,7 +35,24 @@ public abstract class Jogador {
         return jogos;
     }
 
+    public int getJogo(){
+        return jogo;
+    }
+
+    public int getTipo(){
+        return tipo;
+    }
+
     public void jogarDados(){
-        // jogos[jogo] = new JogoDados();
+        if(jogo < 10){
+            jogos[jogo].rolarDados();
+            jogos[jogo].mostrarDados(); //mostrar os dados na tela
+            jogo++;
+        }
+    }
+
+    public void mostrarJogadasExecutadas(){
+        for(int i = 0; i < jogo; i++)
+            jogos[i].getDados();
     }
 }
